eb996e94ef0023b3c5e2dd72aa4f364e7adf1dcd
Jasterix <ljcalixte@gmail.com>
2019-12-01 19:57:05 -0500

    too many active changes

---
+++ ch4/ch4.js
@@ -0,0 +1,15 @@
+export default const sumRange = () => {
+  return "One for you, one for me.";
+};
+
+// export const reverseArray = () => {
+//   sd;
+// };
+
+// export const aList = () => {
+//   return;
+// };
+
+// export const deepComparison = () => {
+//   return;
+// };
